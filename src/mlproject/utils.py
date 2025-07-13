335e8f7e7b86e73d22be0e6df4de4552d2b2f2d5
Anjali Sharma <anjalivsharma2006@gmail.com>
2025-07-13 23:00:19 +0530

    Data Transformation

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -1,33 +1,21 @@
-import os 
+import os
 import sys
+
+import numpy as np 
+import pandas as pd
+import dill
+
 from src.mlproject.exception import CustomException
-from src.mlproject.logger import logging
-import pandas as pd 
-from dotenv import load_dotenv
-import pymysql
 
 
-load_dotenv()
-
-host=os.getenv("host")
-user=os.getenv("user")
-password=os.getenv("password")
-db=os.getenv('db')
-
-def read_sql_data():
-    logging.info("Reading SQL database started")
+def save_object(file_path, obj):
     try:
-        mydb = pymysql.connect(
-            host=host,
-            user=user,
-            password=password,
-            db=db
-        )
-        logging.info("Connection established",mydb)
-        df=pd.read_sql_query('select * from student',mydb)
-        print(df.head())
+        dir_path = os.path.dirname(file_path)
 
-        return df
+        os.makedirs(dir_path, exist_ok=True)
 
-    except Exception as ex:
-        raise CustomException(ex)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
